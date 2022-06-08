9075a1186f98fcea25e820707814e7ab41db39da
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-06-08 15:14:58 -0500

    Finish Tabs & TabsContainer components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent
   ]
   /* providers array, is an array of services
   providers: [ModalService]
